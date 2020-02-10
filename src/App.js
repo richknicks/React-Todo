5cd9a78a9af28301fcecab4e6a8432dd0929e9e0
richknicks <rlynch@gmail.com>
2020-02-10 15:32:49 -0500

    add const myList and constructor function to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+const myList =[
+  {
+    task:'',
+    id:'Date.now()',
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      myTodoList: myList
+    };
+  }
   render() {
     return (
       <div>
