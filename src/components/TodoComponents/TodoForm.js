8c1a7098f2efca83bfb066cbadf1efe9304d5e86
richknicks <rlynch@gmail.com>
2020-02-10 15:42:00 -0500

    created TodoForm

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+const TodoForm = () =>{
+    return (
+    <div> 
+       <form action="">
+           <label htmlFor="listItem">
+               <input type="text" id="listItem" name="todoItem"/>
+           </label>
+           </form> 
+    </div>
+    )}; 
+
+export default TodoForm;
